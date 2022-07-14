e731223b165ad25724af29ad561fa0569df7f260
mgoscar <mgoscar@gmail.com>
2022-07-13 21:47:43 -0500

    re-organizando carpetas y archivos

--- 03_todo_machine/src/components/App/index.jsx
+++ 03_todo_machine/src/components/App/index.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
 
 //import './App.css';
-
+/*
 const defaultTodos = [
   { text: 'Terminar de llenar formato de VISA', completed: true },
   { text: 'Terminar curso de Intro a React', completed: false },
   { text: 'Continuar con curso básico de Inglés', completed: true },
 ];
-
+*/
 function App() {
+  // Traemos nuestros TODOs almacenados
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    // Si existen TODOs en el localStorage los regresamos como nuestros todos
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+
   // Estado inicial de nuestros TODOs
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = React.useState(parsedTodos);
   // El estado de nuestra búsqueda
   const [valorBuscar, setValorBuscar] = React.useState('');
   // Cantidad de TODOs completados
@@ -36,22 +50,31 @@ function App() {
     });
   }
 
+  // Creamos la función en la que actualizaremos nuestro localStorage
+  const saveTodos = (newTodos) => {
+    // Convertimos a string nuestros TODOs
+    const stringifiedTodos = JSON.stringify(newTodos);
+    // Los guardamos en el localStorage
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    // Actualizamos nuestro estado
+    setTodos(newTodos);
+  };
+  
   /**
  * TOGGLE TODO COMPLETED/UNCOMPLETED
  * @param {text} text 
  */
-const toggleCompleteTodo = (text) => {  
-  setTodos(//* Update the state of todos with setTodos    
-    todos.map(todo => { //* Filter the todos array      
-      if (todo.text === text) {//* If the text of the todo matches the text of the todo that was clicked
-        return {          
-          ...todo, //* ... Include the rest of the todo array
-          completed: !todo.completed,//* And toggle the completed property
-        };
-      }
-      return todo; //* Return the rest of the todos array unchanged
-    })
-  );
+const toggleCompleteTodo = (text) => {    
+  const newTodos = todos.map(todo => { //* Filter the todos array      
+    if (todo.text === text) {//* If the text of the todo matches the text of the todo that was clicked
+      return {          
+        ...todo, //* ... Include the rest of the todo array
+        completed: !todo.completed,//* And toggle the completed property
+      };
+    }
+    return todo; //* Return the rest of the todos array unchanged
+  });  
+  saveTodos(newTodos);
 };
 
 /**
@@ -59,9 +82,10 @@ const toggleCompleteTodo = (text) => {
  * @param {text} text 
  */
 const eliminarTodo = (text) => {  
-  setTodos(todos.filter(//* Update the state of todos with setTodos
+  const newTodos = todos.filter(
     todo => todo.text !== text //* Filter the todos array to remove the todo that was clicked
-  ));
+  );
+  saveTodos(newTodos);
 };
 
   // React sólo permite regresar una etiqueta por componente. Utilizamos React.Fragment ya que es invisible
